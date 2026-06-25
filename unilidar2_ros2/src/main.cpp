d7dc5422c4d9fa226700481c20980b0ad8498bfa
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-06-24 19:58:09 -0700

    Send data to lidar

--- unilidar2_ros2/src/main.cpp
+++ unilidar2_ros2/src/main.cpp
@@ -6,10 +6,22 @@ class Node : public rclcpp::Node
 {
 private:
     Lidar lidar_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
 public:
     Node() : rclcpp::Node("unilidar2_node"), lidar_("192.168.1.2", 6201, "192.168.1.62", 6101)
     {
+        timer_ = create_wall_timer(std::chrono::milliseconds(5), std::bind(&Node::timer_cb, this));
+
+        auto now = get_clock()->now();
+        lidar_.sync_time(now.nanoseconds() / 1000000000, now.nanoseconds() % 1000000000);
+        lidar_.set_work_mode(true, false, false, false, false);
+    }
+
+    void timer_cb()
+    {
+        lidar_.get_imu();
+        lidar_.get_cloud();
     }
 };
 
